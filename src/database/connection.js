2b732677092796e697f7de2ff07d4cf66573cf59
Monzer Omer <monzersmiledev@outlook.com>
2022-03-10 05:25:09 +0000

    added: test endpoint

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,3 +1,5 @@
 const mysql = require("mysql2")
 
-mysql.createConnection({host: "", password: "", })
+let database = mysql.createConnection({ host: "45.84.205.211", password: "Admin1728391234", user: "u658742806_Future_Host", database: "u658742806_Future_Host" })
+
+module.exports = database
--- src/routers/Oauth.js
+++ src/routers/Oauth.js
@@ -1,7 +1,16 @@
 const Oauth2_Router = require("express").Router()
 const {Oauth_Controller, Oauth_login} = require("../controllers/Oauth.controller")
+const discord = require("../helpers/discord")
 
 Oauth2_Router.get('/discord/login', Oauth_login)
 Oauth2_Router.get("/api/discord/callback/:code", Oauth_Controller)
+Oauth2_Router.get('/sup', (req, res)=> {
+    discord.users.send("Hola!").then((done) => {
+        res.end()
+    }).catch((err) => {
+        res.send(err)
+    })
+
+})
 
 module.exports = Oauth2_Router
